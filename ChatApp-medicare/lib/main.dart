e3149af8de50617831f81ea7504383fd9bf3293a
vishwateja <vishwalucky28@gmail.com>
2020-09-12 18:56:38 +0530

    Chat_App-medicare

---
+++ ChatApp-medicare/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'modules/main_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Medicare',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
